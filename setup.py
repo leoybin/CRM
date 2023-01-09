5fc3bf30f4dbdd618ff11d52d45a31c263839734
leo_bin <623374149@qq.com>
2023-01-09 16:07:46 +0800

    添加R包

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='crmCustomer',
+      version='1.0.1',
+      description='test for zip',
+      author='yb',
+      author_email='623374149@qq.com',
+      packages=['crmCustomer'],
+      # packages=find_packages()可替换上个packages,可自动寻找到所有文件，当promax下有多文件夹时
+      # 可用
+      )
+
